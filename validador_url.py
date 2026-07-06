4ad358dbfb445f8fc4943601e9a17255162630ec
BrunoMendes007 <bruno.neduziak.mendes@escola.pr.gov.br>
2026-07-06 10:49:13 +0000

    gg

---
+++ validador_url.py
@@ -0,0 +1,11 @@
+import re
+
+url = 
+padrao_url = ('(http(s)://)?alura.com(.br)?')
+
+        https:www.alura.com.br
+
+if not match:
+        raise ValueError("A URL não é valida.")
+
+print("A URL é valida")
